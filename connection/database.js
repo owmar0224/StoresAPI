880405766b33c4dd5b3cb59830f1623f2752a7e1
Omar Fabian <omar.fabian.022498@gmail.com>
2024-10-13 01:17:53 +0800

    Updated vercel.json and database.js

--- connection/database.js
+++ connection/database.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
     }
 );
 
